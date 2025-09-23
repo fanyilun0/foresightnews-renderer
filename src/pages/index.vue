955cee617895a8ddbc33f5aa283d34308293a371
GitHub Action <action@github.com>
2025-09-23 01:05:19 +0000

    Auto update data and import path 2025-09-23

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { computed, ref } from 'vue'
-import foresightData from '../data/foresight_data_20250922.json'
+import foresightData from '../data/foresight_data_20250923.json'
 import defaultHiddenIds from '../data/hidenId.json'
 import 'dayjs/locale/zh-cn'
 
@@ -153,7 +153,7 @@ function isImportant(item: any) {
 <template>
   <div class="mx-auto px-4 py-4 container">
     <div class="mb-6">
-      <Author time="2025-09-22" />
+      <Author time="2025-09-23" />
       <div class="flex items-center justify-between space-x-4">
         <div class="flex items-center space-x-4">
           <h1 class="text-2xl font-bold">
